54ec4c32a15e869cb0b8e19cb0633cd952f8dc4e
Aly <batcha@hartwick.edu>
2019-11-22 13:31:18 -0500

    More Edits

--- Textbook.py
+++ Textbook.py
@@ -1,5 +1,5 @@
 from Program import Person
-class Textbook:
+class Book:
     def __init__(self, title, firstname, lastname, age, edition, ISBN, publisher, yearpublished, quanity, price):
         self.author = Person(firstname,lastname,age)
         self.title = title
@@ -9,4 +9,23 @@ class Textbook:
         self.yearpublished = yearpublished
         self.quanity = quanity
         self.price = price
+
+    def checkout(self):
+        self.quanity - 1
+
+        if self.quanity < 5:
+            return "WARNING! Book count is low, currently have " + self.quanity
+
+        return "0"
+
+    def checkin(self):
+        self.quanity + 1
+        print("Book has been checked in!")
+
+    def checkInventory(self):
+        print(f"Current inventory is {self.quanity}!")
+
+
+
+
 
