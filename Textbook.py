996ed6fedcae25737f25073d42260327f7db54c2
Aly <batcha@hartwick.edu>
2019-11-22 20:51:33 -0500

    More Edits

--- Textbook.py
+++ Textbook.py
@@ -1,17 +1,17 @@
 from Program import Person
 class Book:
-    def __init__(self, title, firstname, lastname, age, edition, ISBN, publisher, yearpublished, quanity, price):
+    def __init__(self, title, firstname, lastname, age, edition, ISBN, publisher, yearpublished, quantity, price):
         self.author = Person(firstname,lastname,age)
         self.title = title
         self.edition = edition
         self.ISBN = ISBN
         self.publisher = publisher
         self.yearpublished = yearpublished
-        self.quanity = quanity
+        self.quanity = quantity
         self.price = price
 
-    def checkout(self):
-        self.quanity - 1
+    def checkout(self, qty):
+        self.quantity = self.quantity - qty
 
         if self.quanity < 5:
             return "WARNING! Book count is low, currently have " + self.quanity
